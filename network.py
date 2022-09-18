ae41e23e5f1ebce53f86c85bbe10fde8b4d5e447
Shiqi Yang <shiqi.yang147@outlook.com>
2022-09-18 21:28:38 +0200

    remove typo
    bootleneck -> bottleneck

--- network.py
+++ network.py
@@ -8,22 +8,29 @@ import math, pdb
 import torch.nn.utils.weight_norm as weightNorm
 
 
-
 def init_weights(m):
     classname = m.__class__.__name__
-    if classname.find('Conv2d') != -1 or classname.find('ConvTranspose2d') != -1:
+    if classname.find("Conv2d") != -1 or classname.find("ConvTranspose2d") != -1:
         nn.init.kaiming_uniform_(m.weight)
         nn.init.zeros_(m.bias)
-    elif classname.find('BatchNorm') != -1:
+    elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight, 1.0, 0.02)
         nn.init.zeros_(m.bias)
-    elif classname.find('Linear') != -1:
+    elif classname.find("Linear") != -1:
         nn.init.xavier_normal_(m.weight)
         nn.init.zeros_(m.bias)
 
 
-res_dict = {"resnet18":models.resnet18, "resnet34":models.resnet34, "resnet50":models.resnet50, 
-"resnet101":models.resnet101, "resnet152":models.resnet152, "resnext50":models.resnext50_32x4d, "resnext101":models.resnext101_32x8d}
+res_dict = {
+    "resnet18": models.resnet18,
+    "resnet34": models.resnet34,
+    "resnet50": models.resnet50,
+    "resnet101": models.resnet101,
+    "resnet152": models.resnet152,
+    "resnext50": models.resnext50_32x4d,
+    "resnext101": models.resnext101_32x8d,
+}
+
 
 class ResBase(nn.Module):
     def __init__(self, res_name):
@@ -53,9 +60,10 @@ class ResBase(nn.Module):
         x = x.view(x.size(0), -1)
         return x
 
-class feat_bootleneck(nn.Module):
+
+class feat_bottleneck(nn.Module):
     def __init__(self, feature_dim, bottleneck_dim=256, type="ori"):
-        super(feat_bootleneck, self).__init__()
+        super(feat_bottleneck, self).__init__()
         self.bn = nn.BatchNorm1d(bottleneck_dim, affine=True)
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=0.5)
@@ -69,14 +77,15 @@ class feat_bootleneck(nn.Module):
             x = self.bn(x)
         return x
 
+
 class feat_classifier(nn.Module):
     def __init__(self, class_num, bottleneck_dim=256, type="linear"):
         super(feat_classifier, self).__init__()
         self.type = type
-        if type == 'wn':
+        if type == "wn":
             self.fc = weightNorm(nn.Linear(bottleneck_dim, class_num), name="weight")
             self.fc.apply(init_weights)
-        elif type == 'linear':
+        elif type == "linear":
             self.fc = nn.Linear(bottleneck_dim, class_num)
             self.fc.apply(init_weights)
         else:
--- train_source.py
+++ train_source.py
@@ -17,23 +17,31 @@ from utils import *
 
 
 def image_train(resize_size=256, crop_size=224):
-    return transforms.Compose([
-        transforms.Resize((resize_size, resize_size)),
-        transforms.RandomCrop(crop_size),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
+    return transforms.Compose(
+        [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.RandomCrop(crop_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+            ),
+        ]
+    )
 
 
 def image_test(resize_size=256, crop_size=224):
-    return transforms.Compose([
-          transforms.Resize((resize_size, resize_size)),
-          transforms.CenterCrop(crop_size),
-          transforms.ToTensor(),
-          torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-      ])
+    return transforms.Compose(
+        [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.CenterCrop(crop_size),
+            transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+            ),
+        ]
+    )
+
 
 def data_load(args):
     ## prepare data
@@ -46,22 +54,48 @@ def data_load(args):
 
     if args.trte == "val":
         dsize = len(txt_src)
-        tr_size = int(0.7*dsize)
+        tr_size = int(0.7 * dsize)
         print(dsize, tr_size, dsize - tr_size)
-        tr_txt, te_txt = torch.utils.data.random_split(txt_src, [tr_size, dsize - tr_size])
+        tr_txt, te_txt = torch.utils.data.random_split(
+            txt_src, [tr_size, dsize - tr_size]
+        )
     else:
         tr_txt = txt_src
         te_txt = txt_src
 
     dsets["source_tr"] = ImageList(tr_txt, transform=image_train())
-    dset_loaders["source_tr"] = DataLoader(dsets["source_tr"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["source_tr"] = DataLoader(
+        dsets["source_tr"],
+        batch_size=train_bs,
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
     dsets["source_te"] = ImageList(te_txt, transform=image_test())
-    dset_loaders["source_te"] = DataLoader(dsets["source_te"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["source_te"] = DataLoader(
+        dsets["source_te"],
+        batch_size=train_bs,
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
     dsets["target"] = ImageList_idx(txt_tar, transform=image_train())
-    dset_loaders["target"] = DataLoader(dsets["target"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["target"] = DataLoader(
+        dsets["target"],
+        batch_size=train_bs,
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
 
     dsets["test"] = ImageList(txt_test, transform=image_test())
-    dset_loaders["test"] = DataLoader(dsets["test"], batch_size=train_bs*3, shuffle=False, num_workers=args.worker, drop_last=False)
+    dset_loaders["test"] = DataLoader(
+        dsets["test"],
+        batch_size=train_bs * 3,
+        shuffle=False,
+        num_workers=args.worker,
+        drop_last=False,
+    )
 
     return dset_loaders
 
@@ -69,20 +103,26 @@ def data_load(args):
 def train_source(args):
     dset_loaders = data_load(args)
     ## set base network
-    if args.net[0:3] == 'res':
+    if args.net[0:3] == "res":
         netF = network.ResBase(res_name=args.net).cuda()
 
-    netB = network.feat_bootleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
-    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
+    netB = network.feat_bottleneck(
+        type=args.classifier,
+        feature_dim=netF.in_features,
+        bottleneck_dim=args.bottleneck,
+    ).cuda()
+    netC = network.feat_classifier(
+        type=args.layer, class_num=args.class_num, bottleneck_dim=args.bottleneck
+    ).cuda()
 
     param_group = []
     learning_rate = args.lr
     for k, v in netF.named_parameters():
-        param_group += [{'params': v, 'lr': learning_rate*0.1}]#1
+        param_group += [{"params": v, "lr": learning_rate * 0.1}]  # 1
     for k, v in netB.named_parameters():
-        param_group += [{'params': v, 'lr': learning_rate*1}]#10
+        param_group += [{"params": v, "lr": learning_rate * 1}]  # 10
     for k, v in netC.named_parameters():
-        param_group += [{'params': v, 'lr': learning_rate*1}]#10
+        param_group += [{"params": v, "lr": learning_rate * 1}]  # 10
     optimizer = optim.SGD(param_group, momentum=0.9, weight_decay=5e-4, nesterov=True)
 
     acc_init = 0
@@ -102,20 +142,30 @@ def train_source(args):
         iter_num += 1
         inputs_source, labels_source = inputs_source.cuda(), labels_source.cuda()
         outputs_source = netC(netB(netF(inputs_source)))
-        classifier_loss = loss.CrossEntropyLabelSmooth(num_classes=args.class_num, epsilon=args.smooth)(outputs_source, labels_source)
+        classifier_loss = loss.CrossEntropyLabelSmooth(
+            num_classes=args.class_num, epsilon=args.smooth
+        )(outputs_source, labels_source)
         optimizer.zero_grad()
         classifier_loss.backward()
         optimizer.step()
 
-        if (iter_num % int(args.interval*len(dset_loaders["source_tr"])) == 0):
+        if iter_num % int(args.interval * len(dset_loaders["source_tr"])) == 0:
             netF.eval()
             netB.eval()
             netC.eval()
-            acc_s_te, acc_list = cal_acc(dset_loaders['source_te'], netF, netB, netC, args.dset=="visda17")
-            log_str = 'Task: {}, Iter:{}; Accuracy = {:.2f}%'.format(args.name_src, iter_num, acc_s_te) + '\n' + str(acc_list)
-            args.out_file.write(log_str + '\n')
+            acc_s_te, acc_list = cal_acc(
+                dset_loaders["source_te"], netF, netB, netC, args.dset == "visda17"
+            )
+            log_str = (
+                "Task: {}, Iter:{}; Accuracy = {:.2f}%".format(
+                    args.name_src, iter_num, acc_s_te
+                )
+                + "\n"
+                + str(acc_list)
+            )
+            args.out_file.write(log_str + "\n")
             args.out_file.flush()
-            print(log_str+'\n')
+            print(log_str + "\n")
             if acc_s_te >= acc_init:
                 acc_init = acc_s_te
                 best_netF = netF.state_dict()
@@ -130,30 +180,43 @@ def train_source(args):
     torch.save(best_netC, osp.join(args.output_dir_src, "source_C_val.pt"))
     return netF, netB, netC
 
-def test_target(args, zz = ''):
+
+def test_target(args, zz=""):
     dset_loaders = data_load(args)
     ## set base network
-    if args.net[0:3] == 'res':
+    if args.net[0:3] == "res":
         netF = network.ResBase(res_name=args.net).cuda()
 
-    netB = network.feat_bootleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
-    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
+    netB = network.feat_bottleneck(
+        type=args.classifier,
+        feature_dim=netF.in_features,
+        bottleneck_dim=args.bottleneck,
+    ).cuda()
+    netC = network.feat_classifier(
+        type=args.layer, class_num=args.class_num, bottleneck_dim=args.bottleneck
+    ).cuda()
 
-    args.modelpath = args.output_dir_src + '/source_F_' + str(zz) + '.pt'
+    args.modelpath = args.output_dir_src + "/source_F_" + str(zz) + ".pt"
     netF.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir_src + '/source_B_' + str(zz) + '.pt'
+    args.modelpath = args.output_dir_src + "/source_B_" + str(zz) + ".pt"
     netB.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir_src + '/source_C_' + str(zz) + '.pt'
+    args.modelpath = args.output_dir_src + "/source_C_" + str(zz) + ".pt"
     netC.load_state_dict(torch.load(args.modelpath))
     netF.eval()
     netB.eval()
     netC.eval()
 
-    acc, acc_list = cal_acc(dset_loaders['test'], netF, netB, netC, args.dset=="visda17")
-    log_str = '\nZz: {}, Task: {}, Accuracy = {:.2f}%'.format(zz, args.name, acc) + '\n' + str(acc_list)
-    args.out_file.write(log_str + '\n')
+    acc, acc_list = cal_acc(
+        dset_loaders["test"], netF, netB, netC, args.dset == "visda17"
+    )
+    log_str = (
+        "\nZz: {}, Task: {}, Accuracy = {:.2f}%".format(zz, args.name, acc)
+        + "\n"
+        + str(acc_list)
+    )
+    args.out_file.write(log_str + "\n")
     args.out_file.flush()
-    print(log_str+'\n')
+    print(log_str + "\n")
 
 
 def print_args(args):
@@ -164,37 +227,47 @@ def print_args(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='BAIT on VisDA')
-    parser.add_argument('--gpu_id', type=str, nargs='?', default='1', help="device id to run")
-    parser.add_argument('--s', type=int, default=0, help="source")
-    parser.add_argument('--t', type=int, default=1, help="target")
-    parser.add_argument('--batch_size', type=int, default=128, help="batch_size")
-    parser.add_argument('--worker', type=int, default=4, help="number of workers")
-    parser.add_argument('--dset', type=str, default='visda-2017', choices=['visda-2017'])
-    parser.add_argument('--lr', type=float, default=1e-3, help="learning rate")
-    parser.add_argument('--net', type=str, default='resnet101', help="resnet50, resnet101")
-    parser.add_argument('--seed', type=int, default=2020, help="random seed")
+    parser = argparse.ArgumentParser(description="BAIT on VisDA")
+    parser.add_argument(
+        "--gpu_id", type=str, nargs="?", default="1", help="device id to run"
+    )
+    parser.add_argument("--s", type=int, default=0, help="source")
+    parser.add_argument("--t", type=int, default=1, help="target")
+    parser.add_argument("--batch_size", type=int, default=128, help="batch_size")
+    parser.add_argument("--worker", type=int, default=4, help="number of workers")
+    parser.add_argument(
+        "--dset", type=str, default="visda-2017", choices=["visda-2017"]
+    )
+    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate")
+    parser.add_argument(
+        "--net", type=str, default="resnet101", help="resnet50, resnet101"
+    )
+    parser.add_argument("--seed", type=int, default=2020, help="random seed")
 
-    parser.add_argument('--max_epoch', type=int, default=5, help="max iterations")
-    parser.add_argument('--interval', type=float, default=0.2, help="max iterations")
+    parser.add_argument("--max_epoch", type=int, default=5, help="max iterations")
+    parser.add_argument("--interval", type=float, default=0.2, help="max iterations")
 
-    parser.add_argument('--bottleneck', type=int, default=256)
-    parser.add_argument('--epsilon', type=float, default=1e-5)
-    parser.add_argument('--layer', type=str, default="wn", choices=["linear", "wn"])
-    parser.add_argument('--classifier', type=str, default="bn", choices=["ori", "bn"])
-    parser.add_argument('--smooth', type=float, default=0.1)
-    parser.add_argument('--output', type=str, default='bait')
-    parser.add_argument('--da', type=str, default='uda')
-    parser.add_argument('--trte', type=str, default='val', choices=['full', 'val'])
-    parser.add_argument('--zz', type=str, default='val', choices=['5', '10', '15', '20', '25', '30', 'val'])
-    parser.add_argument('--savename', type=str, default='bait')
+    parser.add_argument("--bottleneck", type=int, default=256)
+    parser.add_argument("--epsilon", type=float, default=1e-5)
+    parser.add_argument("--layer", type=str, default="wn", choices=["linear", "wn"])
+    parser.add_argument("--classifier", type=str, default="bn", choices=["ori", "bn"])
+    parser.add_argument("--smooth", type=float, default=0.1)
+    parser.add_argument("--output", type=str, default="bait")
+    parser.add_argument("--da", type=str, default="uda")
+    parser.add_argument("--trte", type=str, default="val", choices=["full", "val"])
+    parser.add_argument(
+        "--zz",
+        type=str,
+        default="val",
+        choices=["5", "10", "15", "20", "25", "30", "val"],
+    )
+    parser.add_argument("--savename", type=str, default="bait")
     args = parser.parse_args()
 
     args.interval = args.max_epoch / 10
 
-    names = ['train', 'validation']
+    names = ["train", "validation"]
     args.class_num = 12
-
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
     SEED = args.seed
@@ -202,31 +275,38 @@ if __name__ == "__main__":
     torch.cuda.manual_seed(SEED)
     np.random.seed(SEED)
     random.seed(SEED)
-    #torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.deterministic = True
 
-    folder = './data/'
-    args.s_dset_path = folder + args.dset + '/' + names[args.s] + '_list.txt'
-    args.t_dset_path = folder + args.dset + '/' + names[args.t] + '_list.txt'
+    folder = "./data/"
+    args.s_dset_path = folder + args.dset + "/" + names[args.s] + "_list.txt"
+    args.t_dset_path = folder + args.dset + "/" + names[args.t] + "_list.txt"
     args.test_dset_path = args.t_dset_path
 
     current_folder = "./ckps/"
-    args.output_dir_src = osp.join(current_folder, args.da, args.output, args.dset, names[args.s][0].upper())
+    args.output_dir_src = osp.join(
+        current_folder, args.da, args.output, args.dset, names[args.s][0].upper()
+    )
     args.name_src = names[args.s][0].upper()
     if not osp.exists(args.output_dir_src):
-        os.system('mkdir -p ' + args.output_dir_src)
+        os.system("mkdir -p " + args.output_dir_src)
     if not osp.exists(args.output_dir_src):
         os.mkdir(args.output_dir_src)
 
-    args.output_dir = osp.join(current_folder, args.da, args.output, args.dset, names[args.s][0].upper() + names[args.t][0].upper())
+    args.output_dir = osp.join(
+        current_folder,
+        args.da,
+        args.output,
+        args.dset,
+        names[args.s][0].upper() + names[args.t][0].upper(),
+    )
     args.name = names[args.s][0].upper() + names[args.t][0].upper()
     if not osp.exists(args.output_dir):
-        os.system('mkdir -p ' + args.output_dir)
+        os.system("mkdir -p " + args.output_dir)
     if not osp.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
-    args.out_file = open(osp.join(args.output_dir_src, 'log.txt'), 'w')
-    args.out_file.write(print_args(args)+'\n')
+    args.out_file = open(osp.join(args.output_dir_src, "log.txt"), "w")
+    args.out_file.write(print_args(args) + "\n")
     args.out_file.flush()
     train_source(args)
-    test_target(args, 'val')
-
+    test_target(args, "val")
